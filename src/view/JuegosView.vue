76ea31209c394e25bb7ba9426f698316cfbfe5ff
gonzalofernandezri <gonzalo.fernandezr@elorrieta-errekamari.com>
2026-01-27 19:47:27 +0100

    Margenes

--- src/view/JuegosView.vue
+++ src/view/JuegosView.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="flex flex-col items-center min-h-screen p-4">
-       <div class="w-full flex justify-center mb-4 ">
-            <input @input="juegosFiltrados" type="text" v-model="busqueda" placeholder="Buscar juego..." class="border p-2 rounded w-285">
-       </div>
-       <div class="w-full max-w-6xl">
-       <ul class="grid grid-cols-3 gap-5 ">
-            <li v-for="juego in juegos"  :key="juego.id" class="border p-2 rounded shadow hover:shadow-lg transition">
-                <div>
-                    <img :src="'../../gamefest_resources/games/'+ juego.imagen" :alt="'Imagen de ' + juego.titulo"  class="w-full h-40 object-cover rounded" @click="abrirModal('../../gamefest_resources/games/'+ juego.imagen)">
-                </div>
-                <h3 class="font-sans md:font-serif ">Titulo: {{ juego.titulo }}</h3>
-                <p class="font-sans md:font-serif">Plataforma: {{ JSON.parse(juego.plataformas).join(', ') }}  </p>   
-                <p class="font-sans md:font-serif">Género: {{ juego.genero }} </p>
-                <p class="font-sans md:font-serif">Descripcón: {{ juego.descripcion }}</p>    
-            </li>
-           
-        </ul>
-       </div>
-      
-
+    <div class="flex items-center justify-center grow bg-gray-100 pt-30">
+        <div class="flex flex-col items-center min-h-screen p-4">
+            <div class="w-full flex justify-center mb-4 ">
+                    <input @input="juegosFiltrados" type="text" v-model="busqueda" placeholder="Buscar juego..." class="border p-2 rounded w-285">
+            </div>
+            <div class="w-full max-w-6xl">
+            <ul class="grid grid-cols-3 gap-5 ">
+                    <li v-for="juego in juegos"  :key="juego.id" class="border p-2 rounded shadow hover:shadow-lg transition">
+                        <div>
+                            <img :src="'../../gamefest_resources/games/'+ juego.imagen" :alt="'Imagen de ' + juego.titulo"  class="w-full h-40 object-cover rounded" @click="abrirModal('../../gamefest_resources/games/'+ juego.imagen)">
+                        </div>
+                        <h3 class="font-sans md:font-serif ">Titulo: {{ juego.titulo }}</h3>
+                        <p class="font-sans md:font-serif">Plataforma: {{ JSON.parse(juego.plataformas).join(', ') }}  </p>   
+                        <p class="font-sans md:font-serif">Género: {{ juego.genero }} </p>
+                        <p class="font-sans md:font-serif">Descripcón: {{ juego.descripcion }}</p>    
+                    </li>
+                
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
