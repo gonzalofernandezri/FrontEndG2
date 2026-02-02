a4177e2f7ca772bb2aa92d16ef1bfdf54038401f
gonzalofernandezri <gonzalo.fernandezr@elorrieta-errekamari.com>
2026-02-02 18:46:35 +0100

    tabla

--- src/view/Perfiles.vue
+++ src/view/Perfiles.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center grow bg-gray-100 fondo sm:py-10">
     
-    <div class="w-full max-w-6xl bg-gradient-to-r from-blue-400/50 to-purple-400/50 text-white size-100 font-black text-center rounded-lg border-black p-6 backdrop-blur-sm">
+    <div class="w-full max-w-6xl bg-gradient-to-r from-blue-400/50 to-purple-400/50 text-white size-100 font-black text-center rounded-lg border-black p-6 backdrop-blur-sm h-[600px]">
 
 
       <h4 class="text-3xl font-bold text-center mb-8">
@@ -54,7 +54,7 @@
         </div>
 
         <!-- COLUMNA DERECHA -->
-        <div class="md:col-span-2">
+        <div class="md:col-span-2 flex flex-col h-[445px]">
 
           <h3 class="text-2xl font-bold mb-4 text-white">
             Mis eventos
